a270b425c1155b718bd1eb5762b88de31e43ff48
briannasusz <susz.brianna@gmail.com>
2017-07-26 15:32:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function openCity(cityName, elmnt, color) {
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
 }
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function speech() {
+  document.getElementById("mytext").style.display='none';
+  document.getElementById("mytext2").style.display='none';
+  document.getElementById("mytext3").style.display='none';
+  document.getElementById("mytext4").style.display='none';
+  document.getElementById("makaela").innerHTML = "Thank you for donating!";
+}
+function speech1() {
+  document.getElementById("mytext5").style.display='none';
+  document.getElementById("mytext6").style.display='none';
+  document.getElementById("mytext7").style.display='none';
+  document.getElementById("mytext8").style.display='none';
+  document.getElementById("mytext9").style.display='none';
+  document.getElementById("mytext10").style.display='none';
+  document.getElementById("mytext11").style.display='none';
+  document.getElementById("brianna").innerHTML ='We will contact you about your volunteer work!';
+
+}
